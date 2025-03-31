414da094e8db3a57a26009ea457d4c76c6f0974b
FJSoler_arvc <f.soler@umh.es>
2025-03-31 14:43:22 +0200

    fix eigenvectors

--- include/AHCPlaneSeg.hpp
+++ include/AHCPlaneSeg.hpp
@@ -161,23 +161,32 @@ struct PlaneSeg {
 				normal[0]=V[0][0];
 				normal[1]=V[1][0];
 				normal[2]=V[2][0];
+
+				eigenvectors[0][0] = V[0][0];
+				eigenvectors[1][0] = V[1][0];
+				eigenvectors[2][0] = V[2][0];
+				eigenvectors[0][1] = V[0][1];
+				eigenvectors[1][1] = V[1][1];
+				eigenvectors[2][1] = V[2][1];
+				eigenvectors[0][2] = V[0][2];
+				eigenvectors[1][2] = V[1][2];
+				eigenvectors[2][2] = V[2][2];
 			} else {
 				normal[0]=-V[0][0];
 				normal[1]=-V[1][0];
 				normal[2]=-V[2][0];
+
+				eigenvectors[0][0] = -V[0][0]; // flip z axis
+				eigenvectors[1][0] = -V[1][0]; // flip z axis
+				eigenvectors[2][0] = -V[2][0]; // flip z axis
+				eigenvectors[0][1] = -V[0][1]; // flip y axis
+				eigenvectors[1][1] = -V[1][1]; // flip y axis
+				eigenvectors[2][1] = -V[2][1]; // flip y axis
+				eigenvectors[0][2] = V[0][2];
+				eigenvectors[1][2] = V[1][2];
+				eigenvectors[2][2] = V[2][2];
 			}
 
-			eigenvectors[0][0] = V[0][0];
-			eigenvectors[1][0] = V[1][0];
-			eigenvectors[2][0] = V[2][0];
-
-			eigenvectors[0][1] = V[0][1];
-			eigenvectors[1][1] = V[1][1];
-			eigenvectors[2][1] = V[2][1];
-
-			eigenvectors[0][2] = V[0][2];
-			eigenvectors[1][2] = V[1][2];
-			eigenvectors[2][2] = V[2][2];
 
 			mse = sv[0]*sc;
 			curvature=sv[0]/(sv[0]+sv[1]+sv[2]);
